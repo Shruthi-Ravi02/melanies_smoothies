7b86621a62c73ae351bead1f600457369d2d1fd3
Shruthi-Ravi02 <shruthi.ravi@cognizant.com>
2026-04-27 23:28:17 +0530

    Update streamlit_app.py
    Added Json to amoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
       
-  #New Section to display smoothiefroot nutrition information
-  import requests  
-  smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-  st.text(smoothiefroot_response)
+        #New Section to display smoothiefroot nutrition information
+        import requests  
+        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+        st.text(smoothiefroot_response.json())
